aad26400c16edfd301f102d5396100ef2b18c4ca
Andrew Niziolek <aniziolek9@gmail.com>
2025-04-05 12:02:59 -0400

    Visual Updates, Version Increment, IR from Awin Chip
    Updates to color palette, collapsible styling.

--- scripts/collapse.js
+++ scripts/collapse.js
@@ -2,22 +2,29 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    // Close all other collapsible elements
+  coll[i].addEventListener("click", function () {
+    // Close others
     for (var j = 0; j < coll.length; j++) {
       if (coll[j] !== this && coll[j].classList.contains("active")) {
         coll[j].classList.remove("active");
         coll[j].nextElementSibling.style.maxHeight = null;
+        const otherIcon = coll[j].querySelector(".collapsible-icon");
+        if (otherIcon) otherIcon.textContent = "+";
       }
     }
-
-    // Toggle the clicked collapsible element
+  
+    // Toggle this
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
+  
+    // Update this icon
+    const icon = this.querySelector(".collapsible-icon");
+    if (icon) icon.textContent = this.classList.contains("active") ? "−" : "+";
   });
+  
 }
