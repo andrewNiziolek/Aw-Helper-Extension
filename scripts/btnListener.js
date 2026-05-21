1fa8bc43dfcfac6e64de572f997c2cac0a22c257
Andrew Niziolek <aniziolek9@gmail.com>
2026-05-21 09:07:36 -0400

    Remove newImpl.js, Add IR Tool to Unified Script

--- scripts/btnListener.js
+++ scripts/btnListener.js
@@ -12,20 +12,42 @@ function enterControl(openCol, btnPoint, kbKey) {
     }
 };
 
+
+
 // New Impl Tool
 const implMIDBox = document.getElementById("");
 const implBtn = document.getElementById("");
 const implCollapse = document.getElementById("");
 
-function sendMID(MIDValue) {
+function sendImplMID(MIDValue) {
     chrome.runtime.sendMessage({action: "createNewImplTabs", mid: MIDValue });
 }
 
-implBtn.addEventListener("click", () => {
-    const saniMID = implMIDBox.value.replace(/\D/g, "");
-    if (saniMID) sendOrders(saniMID);
+implBtn?.addEventListener("click", () => {
+    const saniImplMID = implMIDBox.value.replace(/\D/g, "");
+    if (saniImplMID) sendImplMID(saniImplMID);
 });
 
-implCollapse.addEventListener("keydown", (e) => {
+implCollapse?.addEventListener("keydown", (e) => {
     enterControl(implCollapse, implBtn, e);
+});
+
+
+
+// Internal Review Tool
+const irMIDBox = document.getElementById("");
+const irBtn = document.getElementById("");
+const irCollapse = document.getElementById("");
+
+function sendIRMID(MIDValue) {
+    chrome.runtime.sendMessage({ action: "createIRTabs", mid: sanitized });
+}
+
+irBtn?.addEventListener("click", () => {
+    const saniIRMID = irMIDBox.value.replace(/\D/g, "");
+    if (saniIRMID) sendIRMID(saniIRMID);
+});
+
+irCollapse?.addEventListener("keydown", (e) => {
+    enterControl(irCollapse, irBtn, e);
 });
